d587283b56ff105642ea6c58152da085d1aeee5a
Heitorop <verbetskiyarthur@gmail.com>
2021-10-06 12:46:53 +0300

    FirstSlide

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ function check() {
 }
 check();
 checkBo.addEventListener("click", check);
+var swiper = new Swiper(".mySwiper", {
+  effect: "cards",
+  grabCursor: true,
+  slideNextClass: "swiper-slide-next",
+  slidePrevClass: "swiper-slide-prev",
+  navigation: {
+    nextEl: ".swiper-button-next-unique",
+    prevEl: ".swiper-button-prev-unique",
+  },
+});
+
+const nextAr = document.querySelector(".swiper-button-next-unique");
+const prevAr = document.querySelector(".swiper-button-prev-unique");
+
+nextAr.addEventListener("click", function () {
+  if (prevAr.classList.contains("opacity")) {
+    prevAr.classList.remove("opacity");
+  }
+  nextAr.classList.add("opacity");
+});
+
+prevAr.addEventListener("click", function () {
+  if (nextAr.classList.contains("opacity")) {
+    nextAr.classList.remove("opacity");
+  }
+  prevAr.classList.add("opacity");
+});
